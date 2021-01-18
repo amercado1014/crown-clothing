fe5f95ae71b5fb08b8f753b2dcdca8c17a13571d
Aldo Mercado <aldo.mercado@izea.com>
2021-01-17 23:12:53 -0500

    Add ability to add an item to the cart

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleButton, ...otherProps }) => (
-  <button className={`${isGoogleButton ? 'google-button' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({
+  children,
+  isGoogleButton,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''}
+      ${isGoogleButton ? 'google-button' : ''}
+      custom-button`}
+    {...otherProps}
+  >
     {children}
   </button>
-)
+);
 
 export default CustomButton;
